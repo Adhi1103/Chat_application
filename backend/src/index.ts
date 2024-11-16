069c80d8bb60ad5aa55370049f0fb0ae42e642f3
unknown <adarshrajyadav68@gmail.com>
2024-11-16 12:29:35 +0530

    final changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,7 @@ app.use(cors({
     origin: "https://chat-application-k64c.vercel.app", // Replace with your frontend's URL
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
+credentials:true
 }));
 
 app.options("*", cors()); // Handle preflight requests
